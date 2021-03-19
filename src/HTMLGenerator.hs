aa2d30ec1b37a066c81793ae7b726fab9eba6433
fran <franco.sansone@incluit.com>
2021-03-19 17:11:57 -0300

    implementation finished

---
+++ src/HTMLGenerator.hs
@@ -0,0 +1,21 @@
+module HTMLGenerator where 
+import AST
+
+generateHTML :: Markdown -> String
+generateHTML l = concat (map generateHTML1 l)
+
+generateHTML1 :: Block -> String
+generateHTML1 (Header (n, s)) = "<h" ++ show n ++ ">" ++ s ++ "</h>"
+generateHTML1 (List l) = "<ul>" ++ (generateList l) ++ "</ul>"
+generateHTML1 (Paragraph t) = "<p>" ++ (generateParagraph t) ++ "</p>"
+
+generateList :: [String] -> String 
+generateList s = concat (map (\x -> "<li>" ++ x ++ "</li>") s)
+
+generateParagraph :: [Text] -> String
+generateParagraph t = concat (map (\x -> generateParagraph1 x) t)
+
+generateParagraph1 :: Text -> String 
+generateParagraph1 (Bold s) = "<b>" ++ s ++ "</b>"
+generateParagraph1 (Italic s) = "<em>" ++ s ++ "</em>"
+generateParagraph1 (Normal s) = s
--- src/Main.hs
+++ src/Main.hs
@@ -4,19 +4,17 @@ module Main where
 import System.Environment (getArgs)
 import Utils
 import Parser             (parser)
+import HTMLGenerator
 
 
 main :: IO ()
-main = do args <- getArgs
-          if null args then putStrLn ("Error: falta indicar como argumento "
-                                      ++ "la ruta del archivo a ejecutar.")
-                       else do s <- readFile (head args)
-                               print( parser (lines s))
-                        
-                                
-                        
-
--- Ejecuta un programa a partir de su archivo fuente
---run :: [Char] -> IO ()
---run ifile = do s <- readFile ifile
-               --print (eval (parser s))
+main = do 
+        args <- getArgs
+        case args of 
+                [] -> putStrLn ("Error: falta indicar como argumento "
+                                      ++ "la ruta del archivo de entrada y de salida.")
+                filename:[] -> putStrLn ("Error: falta indicar como argumento "
+                                      ++ "la ruta del archivo de salida.")
+                input:[output] -> do s <- readFile input
+                                     writeFile output (generateHTML( parser (lines s)))
+                _ -> putStrLn ("Error: cantidad incorrecta de argumentos.")
